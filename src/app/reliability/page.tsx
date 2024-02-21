9d793aa186f489cb68677aa8d23287609c53d0ea
richfjiq <rfjiq1986@gmail.com>
2024-02-20 18:07:45 -0600

    chore: adds styles

--- src/app/reliability/page.tsx
+++ src/app/reliability/page.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import Hero from '@/components/hero';
+import reliabilityImg from 'public/reliability.jpg';
 
 const ReliabilityPage = () => {
-  return <div>Reliability Page</div>;
+  return (
+    <Hero
+      imgData={reliabilityImg}
+      imgAlt="welding"
+      title="Super high reliability hosting"
+    />
+  );
 };
 
 export default ReliabilityPage;
